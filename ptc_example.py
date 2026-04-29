9fdb443901e9019cfd362d5edd89d20360de3ebd
J.D. Rogers <rogersjd@gmail.com>
2026-04-29 13:06:03 -0500

    Create ptc_example.py
    added an example script for processing data

---
+++ ptc_example.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 11:19:17 2026
+
+@author: jeremy.rogers@wisc.edu
+
+example script for processing PTC
+"""
+
+# %% Imports
+import numpy as np
+import photontransfercurve as PTC
+import matplotlib.pyplot as plt
+
+
+# %% simulate data
+
+
+
+# %% Load small example data
+
+
